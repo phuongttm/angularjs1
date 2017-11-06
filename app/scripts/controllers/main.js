3bf0c05ef5a8cdf040072cb0fe764d9b4eef5845
DINH <thi-mai-phuong.tran@atos.net>
2017-11-06 14:41:01 +0100

    premier

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name formationAngularJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the formationAngularJsApp
+ */
+angular.module('formationAngularJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
